7633e957cfb03ebf571641da614622e1bbae82e9
unknown <ismael.gomez@ou.edu>
2022-07-05 22:11:57 -0500

    medium added

--- super_easy.js
+++ super_easy.js
@@ -1,28 +1,28 @@
 // super easy
-// var num1=6;
-// var num2=2;
+var num1=6;
+var num2=2;
 
-// var difference=(num1-num2);
-// console.log("The difference between " + num1 + " and " + num2 + " is " + difference);
+var difference=(num1-num2);
+console.log("The difference between " + num1 + " and " + num2 + " is " + difference);
 
 // easy
-// var name1= "ismael";
-// var name2= "ruby";
-// var length1=name1.length;
-// var length2=name2.length;
+var name1= "ismael";
+var name2= "ruby";
+var length1=name1.length;
+var length2=name2.length;
 
 
-// if (length1 > length2){
-//     longer=name1
-//     shorter=name2
-//     by = length1 - length2
-// } else {
-//     longer=name2
-//     shorter=name1
-//     by = length2 - length1
-// }
+if (length1 > length2){
+    longer=name1
+    shorter=name2
+    by = length1 - length2
+} else {
+    longer=name2
+    shorter=name1
+    by = length2 - length1
+}
 
-// console.log("The name " + longer + " is longer than " + shorter + " by " + by + " characters");
+console.log("The name " + longer + " is longer than " + shorter + " by " + by + " characters");
 
 // medium
 const prompt = require("prompt-sync")({ sigint: true});
