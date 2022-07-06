d5911163367dcfb9e07239c088dae189005b1ee9
unknown <ismael.gomez@ou.edu>
2022-07-05 21:30:59 -0500

    firsttwo problems

---
+++ super_easy.js
@@ -0,0 +1,25 @@
+// super easy
+// var num1=6;
+// var num2=2;
+
+// var difference=(num1-num2);
+// console.log("The difference between " + num1 + " and " + num2 + " is " + difference);
+
+// easy
+var name1= "ismael";
+var name2= "ruby";
+var length1=name1.length;
+var length2=name2.length;
+
+
+if (length1 > length2){
+    longer=name1
+    shorter=name2
+    by = length1 - length2
+} else {
+    longer=name2
+    shorter=name1
+    by = length2 - length1
+}
+
+console.log("The name " + longer + " is longer than " + shorter + " by " + by + " characters");
